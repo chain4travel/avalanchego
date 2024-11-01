e3922f0d598d969484525ddb56ab60e572e5db07
ev.lekht <ev.lekht@gmail.com>
2024-11-01 20:50:33 +0400

    add missing license

--- api/camino.go
+++ api/camino.go
@@ -1,3 +1,6 @@
+// Copyright (C) 2022-2024, Chain4Travel AG. All rights reserved.
+// See the file LICENSE for licensing terms.
+
 package api
 
 import "github.com/ava-labs/avalanchego/utils/formatting"
